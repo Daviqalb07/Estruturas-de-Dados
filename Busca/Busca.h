73343228420587be91e47aeab587d74c4cae4b1e
Davi Queiroz <64502490+Daviqalb07@users.noreply.github.com>
2020-12-10 14:11:03 -0300

    Create Busca.h

---
+++ Busca/Busca.h
@@ -0,0 +1,10 @@
+#define __BUSCA_H__
+#ifdef __BUSCA_H__
+
+#include <iostream>
+#define NULO -1
+ 
+int buscaSequencial(int *v, int valor, int size);
+int buscaBinaria(int *v, int left, int right, int valor);
+
+#endif
